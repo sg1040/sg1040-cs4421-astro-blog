9f8e33dbd291a10bbb66162bf63e8220110c4880
sg1040 <26431599@studentmail.ul.ie>
2026-09-23 14:44:56 +0100

    fix: error in tscongig.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,46 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Atkinson',
+            cssVariable: '--font-atkinson',
+            fallbacks: ['sans-serif'],
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/atkinson-regular.woff'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                    {
+                        src: ['./src/assets/fonts/atkinson-bold.woff'],
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: 'VCR OSD Mono',
+            cssVariable: '--font-vcr-osd',
+            options: {
+                variants: [
+                    {
+                        src: ['./src/assets/fonts/DistantGalaxy.woff2'],
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                    },
+                ],
+            },
+        },
+    ],
 });
